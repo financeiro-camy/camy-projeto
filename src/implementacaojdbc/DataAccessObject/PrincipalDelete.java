3da4928b3a18930c7d968aadb2b07ed5cd13049b
Carlos Alberto <alberto.carlos@alunoifsp.edu.br>
2023-10-10 15:04:47 -0300

    Todos os métodos DAO testados

--- src/implementacaojdbc/DataAccessObject/PrincipalDelete.java
+++ src/implementacaojdbc/DataAccessObject/PrincipalDelete.java
@@ -3,5 +3,13 @@ package implementacaojdbc.DataAccessObject;
 public class PrincipalDelete {
     public static void main(String[] args) {
         
+            ProjetoCofrinhoDAO projetoCofrinhoDAO = new ProjetoCofrinhoDAO(); 
+            Integer idParaExcluir = 1; 
+        
+            projetoCofrinhoDAO.delete(idParaExcluir);
+        
+            System.out.println("Projeto com ID " + idParaExcluir + " excluído com sucesso.");
+    
+        
     }
 }
