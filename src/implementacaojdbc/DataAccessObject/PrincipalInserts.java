68129e72241dfd052a389bb16fb06eb1da1e7327
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2023-10-13 23:49:30 -0300

    Organizando os INSERTS

--- src/implementacaojdbc/DataAccessObject/PrincipalInserts.java
+++ src/implementacaojdbc/DataAccessObject/PrincipalInserts.java
@@ -8,24 +8,11 @@ public class PrincipalInserts {
 
 	public static void main(String[] args) throws SQLException {
         
-        /*ProjetoCofrinhoDAO projetoCofrinhoDAO = new ProjetoCofrinhoDAO();
-
-        LocalDate prazo = LocalDate.of(2023, 4, 20);
-        LocalDate dataCriacao = LocalDate.of(2023, 9, 23);
-
-       ProjetoCofrinho projetoCofrinho = new ProjetoCofrinho (1,1, "Viagem Milão", "A viagem dos meus sonhos", prazo, dataCriacao, 15000.00,true);
-       ProjetoCofrinho projetoCofrinho2 = new ProjetoCofrinho (2,1,"Carro","carro vermelho",prazo,dataCriacao,56000.00,true);
-       ProjetoCofrinho projetoCofrinho3 = new ProjetoCofrinho (3,1,"Carro","carro preto",prazo,dataCriacao,56000.00,true);
-       ProjetoCofrinho projetoCofrinho4 = new ProjetoCofrinho (4,1,"Plushie do Soobin","pelúcia do Soobin do txt",prazo,dataCriacao,220.00,true);
-       ProjetoCofrinho projetoCofrinho5 = new ProjetoCofrinho (5,1,"Caneta","perdi a minha caneta",prazo,dataCriacao,30.00,true);
-    
-        ProjetoCofrinho projetoCriado = projetoCofrinhoDAO.create(projetoCofrinho);
-        ProjetoCofrinho projetoCriado2 = projetoCofrinhoDAO.create(projetoCofrinho2);
-        ProjetoCofrinho projetoCriado3 = projetoCofrinhoDAO.create(projetoCofrinho3);
-        ProjetoCofrinho projetoCriado4 = projetoCofrinhoDAO.create(projetoCofrinho4);
-        ProjetoCofrinho projetoCriado5 = projetoCofrinhoDAO.create(projetoCofrinho5); 
-
-        CategoriaDAO categoriaDAO = new CategoriaDAO();
+      ContasDinheiroDAO contasDinheiroDAO = new ContasDinheiroDAO();
+      ContasDinheiro contasDinheiro1 = new ContasDinheiro(2,1,"Conta Sylvanian",56789.00,LocalDate.of(2023, 10, 13));
+      contasDinheiroDAO.create(contasDinheiro1);
+      
+       CategoriaDAO categoriaDAO = new CategoriaDAO();
 
         Categoria categoria1 = new Categoria (1,1,"Alimentação");
         Categoria categoria2 = new Categoria (2,1,"Saúde");
@@ -34,56 +21,31 @@ public class PrincipalInserts {
         categoriaDAO.create(categoria1);
         categoriaDAO.create(categoria2);
         categoriaDAO.create(categoria3); 
+    
+      ProjetoCofrinhoDAO projetoCofrinhoDAO = new ProjetoCofrinhoDAO();
+
+       ProjetoCofrinho projetoCofrinho = new ProjetoCofrinho (1,1, "Viagem Milão", "A viagem dos meus sonhos", LocalDate.of(2023,10,13), LocalDate.of(2023,10,13), 15000.00,true);
+       ProjetoCofrinho projetoCofrinho2 = new ProjetoCofrinho (2,1,"Carro","carro vermelho",LocalDate.of(2023,12,31),LocalDate.of(2023,10,13),56000.00,true);
+       ProjetoCofrinho projetoCofrinho3 = new ProjetoCofrinho (3,1,"Carro","carro preto",LocalDate.of(2024,9,15),LocalDate.of(2023,10,13),56000.00,true);
+       ProjetoCofrinho projetoCofrinho4 = new ProjetoCofrinho (4,1,"Plushie do Soobin","pelúcia do Soobin do txt",LocalDate.of(2023,11,12),LocalDate.of(2023,10,11),220.00,true);
+       ProjetoCofrinho projetoCofrinho5 = new ProjetoCofrinho (5,1,"Caneta","perdi a minha caneta",LocalDate.of(2025,12,31),LocalDate.of(2023,10,13),30.00,true);
+    
+        ProjetoCofrinho projetoCriado = projetoCofrinhoDAO.create(projetoCofrinho);
+        ProjetoCofrinho projetoCriado2 = projetoCofrinhoDAO.create(projetoCofrinho2);
+        ProjetoCofrinho projetoCriado3 = projetoCofrinhoDAO.create(projetoCofrinho3);
+        ProjetoCofrinho projetoCriado4 = projetoCofrinhoDAO.create(projetoCofrinho4);
+        ProjetoCofrinho projetoCriado5 = projetoCofrinhoDAO.create(projetoCofrinho5); 
 
         RelatorioPCDAO relatorioPCDAO = new RelatorioPCDAO();
-        LocalDate datainsercao = LocalDate.of(2023, 10, 10);
         
-        RelatorioPC relatorioPC = new RelatorioPC(1,1,1,240.0,datainsercao);
+        RelatorioPC relatorioPC = new RelatorioPC(1,1,2,240.0,LocalDate.of(2023,10,13));
+        RelatorioPC relatorioPC2 = new RelatorioPC (12,1,2,12240.0,LocalDate.of(2023,10,13));
+        RelatorioPC relatorioPC3 = new RelatorioPC(34,2,2,12400.0,LocalDate.of(2023,10,13));
 
-        relatorioPCDAO.create(relatorioPC); */
+        relatorioPCDAO.create(relatorioPC); 
+        relatorioPCDAO.create(relatorioPC2); 
+        relatorioPCDAO.create(relatorioPC3); 
 
-        /*LocalDate datainsercao = LocalDate.of(2023, 10, 10);
-        
-        RelatorioPCDAO relatorioPCDAO = new RelatorioPCDAO();
-        /* 
-        RelatorioPC relatorioPC = new RelatorioPC (12,1,1,12240.0,datainsercao);
-        relatorioPCDAO.create(relatorioPC); */
-
-        /*LocalDate datainsercao1 = LocalDate.of(2023, 10, 10);
-        
-        RelatorioPC relatorioPC1 = new RelatorioPC(34,2,1,12400.0,datainsercao);
-
-        relatorioPCDAO.create(relatorioPC1); 
-
-        /*  LocalDate prazo = LocalDate.of(2023, 4, 20);
-        LocalDate dataCriacao = LocalDate.of(2023, 9, 23);
-        ProjetoCofrinho projetoCofrinho = new ProjetoCofrinho (1,1, "Viagem Milão", "A viagem dos meus sonhos", prazo, dataCriacao, 15000.00,true);
-
-         try {
-        Map<String, Double> resultado = relatorioPCDAO.valorArrecadado(projetoCofrinho);
-
-        if (!resultado.isEmpty()) {
-            double totalArrecadado = resultado.get("Projeto");
-            double quantiaRestante = resultado.get("QuantiaRestante");
-            double percentual = resultado.get("Percentual");
-
-            System.out.println("Projeto: " + projetoCofrinho.getNome());
-            System.out.println("Total arrecadado: " + totalArrecadado);
-            System.out.println("Quantia restante: " + quantiaRestante);
-            System.out.println("Percentual: " + percentual + "%");
-        } else {
-            System.out.println("Nenhum resultado encontrado para o projeto com ID " + projetoCofrinho.getId());
-        }
-    } catch (SQLException e) {
-        e.printStackTrace();
-    }
-
-
-
-      */  
-      ContasDinheiroDAO contasDinheiroDAO = new ContasDinheiroDAO();
-      ContasDinheiro contasDinheiro1 = new ContasDinheiro(2,1,"Conta Sylvanian",56789.00,LocalDate.of(2023, 10, 13));
-      contasDinheiroDAO.create(contasDinheiro1);
     }
 }
 
