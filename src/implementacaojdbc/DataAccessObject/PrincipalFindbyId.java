16c9703bbf80497f262ff58ff19ff85a0d4013ae
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2023-10-14 20:31:07 -0300

    Organizando os testes

--- src/implementacaojdbc/DataAccessObject/PrincipalFindbyId.java
+++ src/implementacaojdbc/DataAccessObject/PrincipalFindbyId.java
@@ -6,16 +6,15 @@ import java.util.Map;
 public class PrincipalFindbyId {
     
     public static void main(String[] args) throws Exception {
-        //ProjetoCofrinhoDAO projetoCofrinhoDAO1 = new ProjetoCofrinhoDAO();
+
+        RelatorioPCDAO relatorioPCDAO = new RelatorioPCDAO();
+        System.out.println(relatorioPCDAO.findById(1));
+
+        ProjetoCofrinhoDAO projetoCofrinhoDAO1 = new ProjetoCofrinhoDAO();
+        System.out.println(projetoCofrinhoDAO1.findById(1));
+        
         CategoriaDAO categoriaDAO = new CategoriaDAO();
-
-        /*System.out.println(projetoCofrinhoDAO1.findById(1));
-        System.out.println(projetoCofrinhoDAO1.findById(2));
-        System.out.println(projetoCofrinhoDAO1.findById(3));
-
-        System.out.println(categoriaDAO.findById(1));
-        System.out.println(categoriaDAO.findById(2));
-        System.out.println(categoriaDAO.findById(3)); */
+         System.out.println(categoriaDAO.findById(1));
 
         ContasDinheiroDAO contasDinheiroDAO = new ContasDinheiroDAO();
         System.out.println(contasDinheiroDAO.findById(2));
